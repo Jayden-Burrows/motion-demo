1bdd4f71f3413b70a7fccbfc12daba01331cabbc
Jayden Burrows <jmb00097@uga.edu>
2026-03-08 20:58:23 -0400

    testing images attempt 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 
+const baseImgPath = process.env.NODE_ENV == 'development'
+  ? ''
+  : '/motion-demo';
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src={baseImgPath + "/next.svg"}
           alt="Next.js logo"
           width={100}
           height={20}
@@ -43,7 +46,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src={baseImgPath + "/vercel.svg"}
               alt="Vercel logomark"
               width={16}
               height={16}

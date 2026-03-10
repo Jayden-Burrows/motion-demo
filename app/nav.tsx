cdec2dbb8ee12e8c067ace5cbaa5caa34487648c
Jayden Burrows <jmb00097@uga.edu>
2026-03-09 23:26:26 -0400

    added functionality to toggleButton

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Link from "next/link";
 const baseImgPath = process.env.NODE_ENV == 'development'
     ? ''    
@@ -7,12 +8,32 @@ type NavProps = {
     link: string;
 };
 
+// Toggle Code From :
+// https://dev.to/abbeyperini/toggle-dark-mode-in-react-28c9
+import { useState } from 'react';
+
 export default function Nav({ link }: NavProps) {
+    const [isLight, setIsLight] = useState(true);
+    const toggleDarkMode = () => {
+        if (isLight) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'light');
+        }
+        setIsLight(() => !isLight)
+    };
     return(
         <div>
             <nav>
-            <p> Po Ping</p>
             <p><Link href={link}>Contact Po Ping</Link></p>
+            <button
+                className="toggleContainerOn"
+                style={{
+                    justifyContent: "flex-" + (isLight ? "start" : "end"),
+                }}
+                onClick={toggleDarkMode}> 
+                <div className="toggleBall"/>
+            </button>
             </nav>
         </div>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Link from "next/link";
 
 import Contact from "./contact/page";
@@ -10,7 +12,15 @@ import Skills from "./skills";
 import Projects from "./projects";
 import Footer from "./footer";
 
+import { useEffect } from 'react';
+// Toggle Code From :
+// https://dev.to/abbeyperini/toggle-dark-mode-in-react-28c9
+import { keepTheme } from './themes';
+
 export default function Home() {
+  useEffect(() => {
+      keepTheme();
+  })
   return (
     <div>
       <Nav link="/contact" />
